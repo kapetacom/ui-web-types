c697da9bbe6aac870d659cd8f114f5e18180a2cc
Jacob Wejendorp <jacob@wejendorp.dk>
2023-05-23 13:48:04 +0200

    fix: add more props to shapeComponents [KAP-515]

--- src/block-providers.ts
+++ src/block-providers.ts
@@ -1,48 +1,61 @@
-import { BlockDefinition, BlockType, LanguageTarget } from '@kapeta/schemas';
-import {ComponentType} from 'react';
-import {ProviderBase} from "./general";
-import {BlockInstance} from "@kapeta/schemas/dist/cjs";
+import { BlockDefinition, BlockType, LanguageTarget } from "@kapeta/schemas";
+import { ComponentType } from "react";
+import { ProviderBase } from "./general";
+import { BlockInstance } from "@kapeta/schemas";
 
-export interface ILanguageTargetProvider<T = any> extends ProviderBase<LanguageTarget> {
-    blockKinds: string[]
-    editorComponent?: ComponentType
-    validate?: (options:T) => string[];
+export interface ILanguageTargetProvider<T = any>
+  extends ProviderBase<LanguageTarget> {
+  blockKinds: string[];
+  editorComponent?: ComponentType;
+  validate?: (options: T) => string[];
 }
 
 export interface BlockTypeEditorProps<T = BlockDefinition> {
-    block: T
-    creating?:boolean
-    readOnly?: boolean;
+  block: T;
+  creating?: boolean;
+  readOnly?: boolean;
 }
 
 export interface BlockTypeConfigProps<T = BlockDefinition> {
-    block: T
-    instance: BlockInstance
-    readOnly?: boolean;
+  block: T;
+  instance: BlockInstance;
+  readOnly?: boolean;
 }
 
 export interface BlockTypeShapeProps<T = BlockDefinition> {
-    block: T
-    instance: BlockInstance
-    readOnly?: boolean;
+  block: T;
+  instance: BlockInstance;
+  height: number;
+  width: number;
+  status?: any;
+  valid?: boolean;
+  readOnly?: boolean;
 }
 
-export interface IBlockTypeProvider<T = BlockDefinition>  extends ProviderBase<BlockType> {
-    //React Component for editing the block definition
-    editorComponent: ComponentType<BlockTypeEditorProps<T>>
+export interface IBlockTypeProvider<T = BlockDefinition>
+  extends ProviderBase<BlockType> {
+  //React Component for editing the block definition
+  editorComponent: ComponentType<BlockTypeEditorProps<T>>;
 
-    //React component that allows overwriting the shape / rendering of the block itself
-    configComponent?: ComponentType<BlockTypeConfigProps<T>>
+  //React component that allows overwriting the shape / rendering of the block itself
+  configComponent?: ComponentType<BlockTypeConfigProps<T>>;
 
-    //Create default configuration for the block
-    createDefaultConfig?: (block: T, instance: BlockInstance) => {[key:string]:any}
+  //Create default configuration for the block
+  createDefaultConfig?: (
+    block: T,
+    instance: BlockInstance
+  ) => { [key: string]: any };
 
-    //React component that allows overwriting the shape / rendering of the block itself
-    shapeComponent?: ComponentType<BlockTypeShapeProps<T>>
+  //React component that allows overwriting the shape / rendering of the block itself
+  shapeComponent?: ComponentType<BlockTypeShapeProps<T>>;
 
-    //Validate block definition. Returns array of errors
-    validate?: (block: T) => string[];
+  //Validate block definition. Returns array of errors
+  validate?: (block: T) => string[];
 
-    //Validate block configuration. Returns array of errors
-    validateConfiguration?: (block: T, instance: BlockInstance, config:{[key:string]:any}) => string[];
+  //Validate block configuration. Returns array of errors
+  validateConfiguration?: (
+    block: T,
+    instance: BlockInstance,
+    config: { [key: string]: any }
+  ) => string[];
 }
