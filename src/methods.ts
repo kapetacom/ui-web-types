c232353b622abed8becf32c674c83365d09b62d3
Soren Mathiasen <sorenm@mymessages.dk>
2023-11-06 14:26:42 +0100

    KAP-1888 Adding arguments to REST methods

--- src/methods.ts
+++ src/methods.ts
@@ -10,6 +10,7 @@ export interface MethodArgument extends TypeLike {}
 
 export interface RESTMethodArgument extends MethodArgument {
     transport?: string;
+    argument?: string;
 }
 
 export interface Method<T extends MethodArgument> {
