1a85f740947c557a712a173df0f40db81c4d43ac
Henrik Hofmeister <henrik@newdawn.dk>
2024-01-20 12:06:24 +0100

    feat: Expands on the resource type provider to allow more flexible types (#19)
    Also adds a context argument for sending related provider handlers to
    the resource provider

--- src/resource-providers.ts
+++ src/resource-providers.ts
@@ -5,8 +5,14 @@
 
 import { ComponentType } from 'react';
 import { Traffic } from './traffic';
-import { BlockDefinition, Connection, Entity, Resource, ResourceType } from '@kapeta/schemas';
+import { BlockDefinition, Connection, Entity, Resource, ResourceMetadata, ResourceType } from '@kapeta/schemas';
 import { ProviderBase } from './general';
+import { IBlockTypeProvider, ILanguageTargetProvider } from './block-providers';
+
+interface ResourceContainerContext {
+    blockProvider: IBlockTypeProvider;
+    languageProvider?: ILanguageTargetProvider;
+}
 
 export enum ResourceRole {
     CONSUMES = 'CONSUMES',
@@ -19,12 +25,12 @@ export enum ResourceProviderType {
     EXTENSION = 'EXTENSION',
 }
 
-export interface ResourceConnectionMappingChange<T = any, U = any, V = any> {
-    source: Resource;
-    sourceEntities: Entity[];
-    target: Resource;
-    targetEntities: Entity[];
-    data: V;
+export interface ResourceConnectionMappingChange<EntityType = Entity, DataType = any, ResourceSpec = any> {
+    source: ResourceWithSpec<ResourceSpec>;
+    sourceEntities: EntityType[];
+    target: ResourceWithSpec<ResourceSpec>;
+    targetEntities: EntityType[];
+    data: DataType;
 }
 
 export enum ConnectionMethodMappingType {
@@ -38,80 +44,115 @@ export interface ConnectionMethodMapping {
 
 export type ConnectionMethodsMapping = { [key: string]: ConnectionMethodMapping };
 
-export interface ResourceTypeProviderMappingProps<T = any, U = any, V = any> {
-    source: Resource;
-    target: Resource;
-    value?: V;
-    sourceEntities: Entity[];
-    targetEntities: Entity[];
+export interface ResourceWithSpec<Spec, Metadata = ResourceMetadata> extends Omit<Resource, 'spec' | 'metadata'> {
+    spec: Spec;
+    metadata: Metadata;
+}
+
+export interface ResourceTypeProviderMappingProps<
+    SourceSpec = any,
+    TargetSpec = any,
+    DataType = any,
+    EntityType = Entity
+> {
+    source: ResourceWithSpec<SourceSpec>;
+    target: ResourceWithSpec<TargetSpec>;
+    value?: DataType;
+    sourceEntities: EntityType[];
+    targetEntities: EntityType[];
     title: string;
-    onDataChanged?: (change: ResourceConnectionMappingChange<T, U, V>) => void;
+    onDataChanged?: (change: ResourceConnectionMappingChange<EntityType, DataType, TargetSpec>) => void;
 }
 
 export interface ResourceTypeProviderInspectorProps {
     trafficLines: Traffic[];
     mapping: ConnectionMethodsMapping;
+    context?: ResourceContainerContext;
 }
 
 export interface ResourceTypeProviderEditorProps {
     block: BlockDefinition;
     creating?: boolean; //True if the resource is new
+    context?: ResourceContainerContext;
 }
 
-export interface IResourceTypeConverter<T = any, V = ConnectionMethodsMapping> {
-    mappingComponentType?: ComponentType<ResourceTypeProviderMappingProps>;
+export interface IResourceTypeConverter<
+    ResourceSpecType = any,
+    MappingType = ConnectionMethodsMapping,
+    EntityType = Entity,
+    MetadataType = ResourceMetadata
+> {
+    mappingComponentType?: ComponentType<
+        ResourceTypeProviderMappingProps<ResourceSpecType, ResourceSpecType, MappingType, EntityType>
+    >;
     inspectComponentType?: ComponentType<ResourceTypeProviderInspectorProps>;
     fromKind: string;
-    createFrom?: (source: Resource) => Resource;
+    createFrom?: (
+        source: ResourceWithSpec<ResourceSpecType, MetadataType>
+    ) => ResourceWithSpec<ResourceSpecType, MetadataType>;
     validateMapping?: (
         connection: Connection,
-        from: Resource,
-        to: Resource,
-        fromEntities: Entity[],
-        toEntities: Entity[]
+        from: ResourceWithSpec<ResourceSpecType, MetadataType>,
+        to: ResourceWithSpec<ResourceSpecType, MetadataType>,
+        fromEntities: EntityType[],
+        toEntities: EntityType[]
     ) => string[];
     updateMapping?: (
         connection: Connection,
-        from: Resource,
-        to: Resource,
-        fromEntities: Entity[],
-        toEntities: Entity[]
-    ) => V;
-    createMapping?: (from: Resource, to: Resource, fromEntities: Entity[], toEntities: Entity[]) => V;
+        from: ResourceWithSpec<ResourceSpecType, MetadataType>,
+        to: ResourceWithSpec<ResourceSpecType, MetadataType>,
+        fromEntities: EntityType[],
+        toEntities: EntityType[]
+    ) => MappingType;
+    createMapping?: (
+        from: ResourceWithSpec<ResourceSpecType, MetadataType>,
+        to: ResourceWithSpec<ResourceSpecType, MetadataType>,
+        fromEntities: EntityType[],
+        toEntities: EntityType[]
+    ) => MappingType;
 }
 
-export interface IResourceTypeProviderConfig<T = any, U = any> {
+export interface IResourceTypeProviderConfig<
+    MetadataType = ResourceMetadata,
+    ResourceSpecType = any,
+    EntityType = Entity
+> {
     //Function that can resolve entities from a resource
-    resolveEntities?: (resource: Resource) => string[];
+    resolveEntities?: (resource: ResourceWithSpec<ResourceSpecType, MetadataType>) => string[];
 
     //Function that can rename entity references in a resource
-    renameEntityReferences?: (resource: Resource, from: string, to: string) => void;
+    renameEntityReferences?: (
+        resource: ResourceWithSpec<ResourceSpecType, MetadataType>,
+        from: string,
+        to: string
+    ) => void;
 
     //List of converters that can convert this kind into other kinds - and also provides mapping UI
-    converters?: IResourceTypeConverter<U>[];
+    converters?: IResourceTypeConverter<ResourceSpecType, ConnectionMethodsMapping, EntityType, MetadataType>[];
 
     //React component for editing the resource type
     editorComponent?: ComponentType<ResourceTypeProviderEditorProps>;
 
     //Function that determines has a counter and what that counter is
-    getCounterValue?: (data: Resource) => number;
+    getCounterValue?: (data: ResourceWithSpec<ResourceSpecType, MetadataType>) => number;
 
     //For resources that provide methods - this function determines if the resource has a specific method
-    hasMethod?: (data: Resource, methodId: string) => boolean;
+    hasMethod?: (data: ResourceWithSpec<ResourceSpecType, MetadataType>, methodId: string) => boolean;
 
     //Validates a resource and returns a list of errors
-    validate?: (data: Resource, entities: Entity[]) => string[];
+    validate?: (data: ResourceWithSpec<ResourceSpecType, MetadataType>, entities: EntityType[]) => string[];
 }
 
-export interface ResourceTypeShapeProps {
-    resource: Resource;
+export interface ResourceTypeShapeProps<ResourceSpecType = any, MetadataType = ResourceMetadata> {
+    resource: ResourceWithSpec<ResourceSpecType, MetadataType>;
     role: ResourceRole;
     index: number;
     readOnly?: boolean;
+    context?: ResourceContainerContext;
 }
 
-export interface IResourceTypeProvider<T = any, U = any>
-    extends IResourceTypeProviderConfig<T, U>,
+export interface IResourceTypeProvider<MetadataType = ResourceMetadata, ResourceSpecType = any, EntityType = Entity>
+    extends IResourceTypeProviderConfig<MetadataType, ResourceSpecType, EntityType>,
         ProviderBase<ResourceType> {
     //Defines the role of the resource - either provider or consumer.
     role: ResourceRole;
@@ -123,5 +164,13 @@ export interface IResourceTypeProvider<T = any, U = any>
     consumableKind?: string;
 
     //React component that allows overwriting the shape / rendering of the resource
-    shapeComponent?: ComponentType<ResourceTypeShapeProps>;
+    shapeComponent?: ComponentType<ResourceTypeShapeProps<ResourceSpecType, MetadataType>>;
+
+    capabilities?: {
+        /**
+         * If true, the resource can be handle and is expecting DSL types directly
+         * instead of the Entity types
+         */
+        directDSL?: boolean;
+    };
 }
